d93c839620a2647e0b04bb22f8a8f2355f36447f
se9oo <se9oo@kakao.com>
2024-11-18 22:47:48 +0900

    feat: Pretendard font 적용

---
+++ app/_font.ts
@@ -0,0 +1,33 @@
+import localFont from 'next/font/local';
+
+export const pretendard = localFont({
+	display: 'swap',
+	variable: '--font-pretendard',
+	src: [
+		{
+			path: '../public/fonts/pretendard/woff2/Pretendard-Light.subset.woff2',
+			weight: '300',
+			style: 'normal',
+		},
+		{
+			path: '../public/fonts/pretendard/woff2/Pretendard-Regular.subset.woff2',
+			weight: '400',
+			style: 'normal',
+		},
+		{
+			path: '../public/fonts/pretendard/woff2/Pretendard-Medium.subset.woff2',
+			weight: '500',
+			style: 'normal',
+		},
+		{
+			path: '../public/fonts/pretendard/woff2/Pretendard-SemiBold.subset.woff2',
+			weight: '600',
+			style: 'normal',
+		},
+		{
+			path: '../public/fonts/pretendard/woff2/Pretendard-Bold.subset.woff2',
+			weight: '700',
+			style: 'normal',
+		},
+	],
+});
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,6 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import type { Metadata } from 'next';
+import '@/public/globals.css';
+import { pretendard } from '@/app/_font';
 
 export const metadata: Metadata = {
 	title: 'linnnk',
@@ -19,17 +8,13 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html lang="ko" className={`${pretendard.variable}`}>
+			<body className={`${pretendard.className} antialiased`}>{children}</body>
+		</html>
+	);
 }
