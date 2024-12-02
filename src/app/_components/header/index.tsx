61341b1c28d2d5717db0377d2f04c36c8a6ce93a
se9oo <se9oo@kakao.com>
2024-12-02 22:21:36 +0900

    feat: Header > 로그아웃 기능 추가

--- src/app/_components/header/index.tsx
+++ src/app/_components/header/index.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { signOut, useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
 
 import Button from '@/app/_components/button';
@@ -9,6 +10,17 @@ import { APP_ROUTES } from '@/constants/routes';
 
 export default function Header() {
 	const router = useRouter();
+	const { data: session, status } = useSession();
+
+	const handleSign = () => {
+		if (session?.user) {
+			signOut();
+			router.push(APP_ROUTES.MAIN);
+			return;
+		}
+
+		router.push(APP_ROUTES.SIGN_IN);
+	};
 
 	return (
 		<header className="h-header flex w-full items-center justify-between bg-white px-4 py-2 shadow-sm">
@@ -16,11 +28,13 @@ export default function Header() {
 				<SVGButton icon="menu" width={32} height={32} strokeWidth={2} />
 				<span className="text-2xl">Linnnk</span>
 			</div>
-			<div className="flex basis-1/3 justify-end">
-				<Button size="sm" onClick={() => router.push(APP_ROUTES.SIGN_IN)}>
-					로그인
-				</Button>
-			</div>
+			{status !== 'loading' && (
+				<div className="flex basis-1/3 justify-end">
+					<Button size="sm" onClick={handleSign}>
+						{!!session?.user ? '로그아웃' : '로그인'}
+					</Button>
+				</div>
+			)}
 		</header>
 	);
 }
