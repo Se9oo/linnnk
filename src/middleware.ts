bcee7f1a582ba3052d663844ea50ddcdc09ac0a0
se9oo <se9oo@kakao.com>
2024-12-02 23:09:44 +0900

    feat: middleware 추가 > 특정 path 진입시 로그인 상태가 아니라면 로그인 페이지로 이동

---
+++ src/middleware.ts
@@ -0,0 +1,16 @@
+import { auth } from '@/libs/auth';
+import { NextRequest, NextResponse } from 'next/server';
+
+import { APP_ROUTES } from '@/constants/routes';
+
+export async function middleware(request: NextRequest) {
+	const session = await auth();
+
+	if (!session) {
+		return NextResponse.redirect(new URL(APP_ROUTES.SIGN_IN, request.url));
+	}
+}
+
+export const config = {
+	matcher: ['/home'],
+};
