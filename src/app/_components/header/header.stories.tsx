070f1c1516b0d0324ea1aa164746d14bddb1f299
se9oo <se9oo@kakao.com>
2024-12-02 22:43:07 +0900

    test: header story > 로그인, 로그아웃 상태 추가

--- src/app/_components/header/header.stories.tsx
+++ src/app/_components/header/header.stories.tsx
@@ -1,6 +1,17 @@
 import type { Meta, StoryObj } from '@storybook/react';
+import { Session } from 'next-auth';
+import { SessionContext } from 'next-auth/react';
 
+import AuthSession from '@/app/_components/auth/auth-session';
 import Header from '@/app/_components/header';
+
+const mockUser: Session = {
+	user: {
+		id: '',
+		email: '',
+	},
+	expires: '',
+};
 
 const meta = {
 	title: 'Components/Header',
@@ -13,4 +24,20 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
 	args: {},
+	render: () => (
+		<AuthSession>
+			<Header />
+		</AuthSession>
+	),
+};
+
+export const AfterSignInHeader: Story = {
+	args: {},
+	render: () => {
+		return (
+			<SessionContext.Provider value={{ status: 'authenticated', data: mockUser, update: async () => null }}>
+				<Header />
+			</SessionContext.Provider>
+		);
+	},
 };
