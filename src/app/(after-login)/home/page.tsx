58dad582ff963e4954079082c80487af0d5f5610
se9oo <se9oo@kakao.com>
2024-12-04 23:40:54 +0900

    feat: Home 페이지 추가

---
+++ src/app/(after-login)/home/page.tsx
@@ -0,0 +1,9 @@
+import AfterLoginLayout from '@/app/_components/layout/after-login-layout';
+
+export default async function Home() {
+	return (
+		<AfterLoginLayout>
+			<div></div>
+		</AfterLoginLayout>
+	);
+}
