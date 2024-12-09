11a7112af90acbab027d1f7c9352d3e8f38c20bb
se9oo <se9oo@kakao.com>
2024-12-09 21:37:16 +0900

    fix: header > 미사용 import 제거

--- src/app/_components/header/index.tsx
+++ src/app/_components/header/index.tsx
@@ -4,7 +4,6 @@ import { signOut, useSession } from 'next-auth/react';
 import { usePathname, useRouter } from 'next/navigation';
 
 import Button from '@/app/_components/button';
-import SVGButton from '@/app/_components/button/svg-button';
 
 import { APP_ROUTES } from '@/constants/routes';
 
@@ -25,7 +24,7 @@ export default function Header() {
 	};
 
 	return (
-		<header className="h-header flex w-full items-center justify-between bg-white px-4 py-2 shadow-sm">
+		<header className="flex h-header w-full items-center justify-between bg-white px-4 py-2 shadow-sm">
 			<span className="text-2xl">Linnnk</span>
 			{pathname !== APP_ROUTES.SIGN_IN && status !== 'loading' && (
 				<div className="flex basis-1/3 justify-end">
