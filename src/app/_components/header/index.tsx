8ca915b04f696dc309aa8383c62d99966ce1c3b0
se9oo <se9oo@kakao.com>
2024-12-12 22:33:41 +0900

    fix: Header > 앱 메인페이지에서만 로그인 버튼 보이도록 수정, 로그아웃 로직 제거

--- src/app/_components/header/index.tsx
+++ src/app/_components/header/index.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { signOut, useSession } from 'next-auth/react';
 import { usePathname, useRouter } from 'next/navigation';
 
 import Button from '@/app/_components/button';
@@ -11,27 +10,17 @@ export default function Header() {
 	const router = useRouter();
 	const pathname = usePathname();
 
-	const { data: session, status } = useSession();
-
-	const handleSign = () => {
-		if (session?.user) {
-			signOut();
-			router.push(APP_ROUTES.MAIN);
-			return;
-		}
-
+	const handleSignIn = () => {
 		router.push(APP_ROUTES.SIGN_IN);
 	};
 
 	return (
 		<header className="flex h-header w-full items-center justify-between bg-white px-4 py-2 shadow-sm">
 			<span className="text-2xl">Linnnk</span>
-			{pathname !== APP_ROUTES.SIGN_IN && status !== 'loading' && (
-				<div className="flex basis-1/3 justify-end">
-					<Button size="sm" onClick={handleSign}>
-						{!!session?.user ? '로그아웃' : '로그인'}
-					</Button>
-				</div>
+			{pathname === APP_ROUTES.MAIN && (
+				<Button size="sm" onClick={handleSignIn}>
+					로그인
+				</Button>
 			)}
 		</header>
 	);
