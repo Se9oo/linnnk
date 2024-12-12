dd9538be289c963a0eaf682980f953da1ee35e14
se9oo <se9oo@kakao.com>
2024-12-12 22:34:23 +0900

    feat: AccountAction > 로그아웃 로직 추가 및 연동된 서비스 icon 추가

--- src/app/_components/menu/account-action.tsx
+++ src/app/_components/menu/account-action.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useSession } from 'next-auth/react';
+import { signOut, useSession } from 'next-auth/react';
 
 import BottomSheet from '@/app/_components/bottom-sheet/bottom-sheet';
 import Button from '@/app/_components/button';
@@ -8,17 +8,34 @@ import Button from '@/app/_components/button';
 export default function AccoutAction() {
 	const { data: session } = useSession();
 
+	const userImage = session?.user.image;
+	const provider = session?.user.provider;
+	const username = session?.user.name;
+
+	const handleSignOut = () => {
+		signOut();
+	};
+
 	return (
 		<BottomSheet className="h-fit">
 			<div className="flex gap-4">
-				<img src={session?.user?.image || ''} alt="user image" className="h-fit w-20 rounded-full" />
+				<img src={userImage || ''} alt="user image" className="h-fit w-20 rounded-full" />
 				<div className="items-stretch">
-					<span className="block text-2xl font-bold">{session?.user?.name}</span>
+					<span className="block text-2xl font-bold">{username}</span>
 					<span className="mt-4 block font-semibold">연동된 서비스</span>
-					<span>{session?.user?.provider}</span>
+					<div className="mt-2 flex items-center gap-2">
+						<img
+							src={`/assets/images/${provider}-symbol.${provider === 'kakao' ? 'png' : 'svg'}`}
+							alt="provider symbol"
+							className="w-5"
+						/>
+						<span>{provider}</span>
+					</div>
 				</div>
 			</div>
-			<Button className="mt-10 w-full">로그아웃</Button>
+			<Button className="mt-10 w-full" onClick={handleSignOut}>
+				로그아웃
+			</Button>
 		</BottomSheet>
 	);
 }
