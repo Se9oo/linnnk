81c72444c558c3ef2041131f1f62625bf0f30959
se9oo <se9oo@kakao.com>
2024-12-17 22:02:19 +0900

    feat: Modal BackDrop > 열기, 닫기 animation 추가

--- src/app/_components/modal/back-drop.tsx
+++ src/app/_components/modal/back-drop.tsx
@@ -1,7 +1,19 @@
-interface BackDropProps {
-	children: React.ReactNode;
-}
+import { HTMLAttributes } from 'react';
 
-export default function BackDrop({ children }: BackDropProps) {
-	return <div className="fixed left-0 top-0 h-screen w-screen transform-gpu bg-back-drop">{children}</div>;
+import { useActiveModals } from '@/stores/modal';
+
+interface BackDropProps extends HTMLAttributes<HTMLDivElement> {}
+
+export default function BackDrop({ children, onTransitionEnd }: BackDropProps) {
+	const activeModals = useActiveModals();
+
+	return (
+		<div
+			className={`fixed left-0 top-0 h-screen w-screen transform-gpu bg-back-drop transition-opacity duration-200
+				${activeModals.length === 0 ? 'opacity-0' : 'animate-fade-in'}`}
+			onTransitionEnd={onTransitionEnd}
+		>
+			{children}
+		</div>
+	);
 }
--- src/app/_components/modal/modal.tsx
+++ src/app/_components/modal/modal.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { usePathname } from 'next/navigation';
-import { useEffect } from 'react';
+import { TransitionEvent, useEffect, useState } from 'react';
 import { createPortal } from 'react-dom';
 
 import BackDrop from '@/app/_components/modal/back-drop';
@@ -11,17 +11,35 @@ import { useActiveModals, useModalActions } from '@/stores/modal';
 export default function Modal() {
 	const pathname = usePathname();
 
+	// modal state
 	const activeModals = useActiveModals();
 	const { clearActiveModals } = useModalActions();
 
+	// modal rendering check
+	const [showModal, setShowModal] = useState(false);
+
+	// backdrop opacity animation 종료시 modal 닫기
+	const handleBackDropTransition = (e: TransitionEvent) => {
+		if (e.target === e.currentTarget && showModal) {
+			setShowModal(false);
+		}
+	};
+
+	// 경로 변경시 열려 있는 modal 초기화
 	useEffect(() => {
 		clearActiveModals();
 	}, [pathname]);
 
-	if (activeModals.length === 0) return null;
+	useEffect(() => {
+		if (!showModal && activeModals.length > 0) {
+			setShowModal(true);
+		}
+	}, [activeModals]);
+
+	if (!showModal && activeModals.length === 0) return null;
 
 	return createPortal(
-		<BackDrop>
+		<BackDrop onTransitionEnd={handleBackDropTransition}>
 			{activeModals.map((modal) => (
 				<div key={modal.id}>{modal.component}</div>
 			))}
