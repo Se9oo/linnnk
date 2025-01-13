8376528d359aa080d8fb182b4d16a23303f80146
se9oo <se9oo@kakao.com>
2025-01-13 22:18:45 +0900

    style: Chip > border 제거, text nowrap 적용

--- src/app/_components/chip/index.tsx
+++ src/app/_components/chip/index.tsx
@@ -8,10 +8,7 @@ interface ChipProps {
 export default function Chip({ name, className }: ChipProps) {
 	return (
 		<span
-			className={cn(
-				'bg-primary-light h-10 rounded-full border border-primary px-4 py-2 font-semibold text-primary',
-				className,
-			)}
+			className={cn('bg-primary-light h-10 text-nowrap rounded-full px-4 py-2 font-semibold text-primary', className)}
 		>
 			{name}
 		</span>
